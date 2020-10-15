e4f6d203a602ae7f45dd444b4fb4b7a2f9792ae8
Raúl González <gonzalv@ciencias.unam.mx>
2020-10-14 20:43:09 -0500

    [MODIFIED] adaptado a la especificación nueva

--- Compiler.rkt
+++ Compiler.rkt
@@ -14,13 +14,20 @@ Lexer y parser
          (prefix-in re- parser-tools/lex-sre)
          parser-tools/yacc)
 
-(define-tokens a (NUM VAR BOOL TYPE)) ;Tokens que son contenedores --- data Tokens = NUM Int | VAR String | BOOL Bool
-(define-empty-tokens b (+ - * / AND OR EOF LET ASSIGN IN LP RP FUN FUNF IF ARROW LSB RSB END)) ;Tokens que no almacenan datos
+;; Tokens que son contenedores --- data Tokens = NUM Int | VAR String | BOOL Bool
+(define-tokens a (NUM BOOL  ;; Constants
+                      VAR)) ;; Variable
+                      
+;; Tokens que no almacenan datos
+(define-empty-tokens b (+ - * / AND OR          ;; prim
+                          LP RP LSB RSB LB RB   ;; parentesis (), brackets {} y square brackets []
+                          IF THEN ELSE          ;; condicional
+                          BOOLE INT FUNC        ;; Types 
+                          FUN FUNF TYPEOF ARROW ;; Funciones
+                          LET ASSIGN IN END     ;; Let
+                          APP                   ;; Aplicacion de funcion
+                          EOF))                 ;; End of line
 
-; "fun (x:T) : T => x"
-; [FUN,LP,VAR "x",TYPEOF,TYPE "T",RP,TYPEOF,TYPE "T",ACA,VAR "x",ATA,EOF]
-; TYPE (TYPE (TYPE ...))
-; T ::= INT | BOOL | T -> T
 
 ; Esta definicion pertenece a parsers-tools/lex-ptv-v200
 ; pero al importar esta biblioteca, causa problemas de sintaxis con los
@@ -35,80 +42,93 @@ Lexer y parser
    ;[RegularExpression
    ;=>
    ;Token]
+
    
-   ; :: s*
-   [(:: #\l #\e #\t) ; Expresion regular let
+   ;; EMPIEZA relacionado a let
+   [(:: #\l #\e #\t)
     ; =>
     (cons (token-LET)
-          (minHS-lexer input-port))] ;token resultante
-
+          (minHS-lexer input-port))]
    [#\=
     ; =>
     (cons (token-ASSIGN)
           (minHS-lexer input-port))]
-
    [(:: #\i #\n)
     ; =>
     (cons (token-IN)
           (minHS-lexer input-port))]
-
    [(:: #\e #\n #\d)
     ; =>
     (cons (token-END)
           (minHS-lexer input-port))]
+   ;; TERMINA relacionado a let
+   
 
+   ;; EMPIEZA relacionado al if
    [(:: #\i #\f)
     ; =>
     (cons (token-IF)
           (minHS-lexer input-port))]
-
-   [(:: #\# (:or #\t #\f))
+   [(:: #\t #\h #\e #\n)
     ; =>
-    (cons (token-BOOL (equal? lexeme "#t"))
+    (cons (token-THEN)
           (minHS-lexer input-port))]
-
-   [(:or (:: #\B #\o #\o #\l) (:: #\I #\n #\t) (:: #\F #\u #\n #\c))
+   [(:: #\e #\l #\s #\e)
     ; =>
-    (cons (token-TYPE (string->symbol lexeme))
+    (cons (token-ELSE)
           (minHS-lexer input-port))]
+   ;; TERMINA relacionado al if
 
+   
+   ;; EMPIEZA relacionado a tipos
+   [#\:
+    ; =>
+    (cons (token-TYPEOF)
+          (minHS-lexer input-port))]
+   [(:: #\B #\o #\o #\l)
+    ; =>
+    (cons (token-BOOLE)
+          (minHS-lexer input-port))]
+   [(:: #\I #\n #\t)
+    ; =>
+    (cons (token-INT)
+          (minHS-lexer input-port))]
+   [(:: #\F #\u #\n #\c)
+    ; =>
+    (cons (token-FUNC)
+          (minHS-lexer input-port))]
+   ;; TERMINA relacionado a tipos
+
+
+   ;; EMPIEZA relacionado a funciones
    [(:: #\f #\u #\n)
     ; =>
     (cons (token-FUN)
           (minHS-lexer input-port))]
-
    [(:: #\f #\u #\n #\F)
     ; =>
     (cons (token-FUNF)
           (minHS-lexer input-port))]
-
    [(:: #\= #\>)
     ; =>
     (cons (token-ARROW)
           (minHS-lexer input-port))]
-   
-   [(:+ (:: (char-range #\a #\z) (:? (char-range #\0 #\9))))
-    ;;(:+ (:or (char-range #\a #\z) (char-range #\A #\Z))) ; ([a..z]|[A..Z])^+
+   [(:: #\a #\p #\p)
     ; =>
-    (cons (token-VAR (string->symbol lexeme))
-          (minHS-lexer input-port))]
+    (cons (token-APP)
+          (minHS-lexer input-port))] 
+   ;; TERMINA relacionado a funciones
 
-   [(:+ (char-range #\0 #\9))
-    ;; (::  (:or #\- (epsilon)) (:: (:* (char-range #\0 #\9)) (:: (:or (:: #\. (char-range #\0 #\9)) (:: (char-range #\0 #\9)) #\.) (:* (char-range #\0 #\9)))))
-    ; =>
-    (cons (token-NUM (string->number lexeme))
-          (minHS-lexer input-port))]
-   
+
+   ;; EMPIEZA operadores prim
    [#\+
     ; =>
     (cons (token-+)
           (minHS-lexer input-port))]
-
    [#\-
     ; =>
     (cons (token--)
           (minHS-lexer input-port))]
-
    [#\*
     ; =>
     (cons (token-*)
@@ -117,7 +137,8 @@ Lexer y parser
     ; =>
     (cons (token-/)
           (minHS-lexer input-port))]
-   [(:: #\a #\n #\d)
+
+    [(:: #\a #\n #\d)
     ; =>
     (cons (token-AND)
           (minHS-lexer input-port))]
@@ -125,31 +146,63 @@ Lexer y parser
     ; =>
     (cons (token-OR)
           (minHS-lexer input-port))]
+   ;; TERMINA operadores prim
+   
+   
+   ;; EMPIEZA constantes
+   [(:+ (char-range #\0 #\9))
+    ; =>
+    (cons (token-NUM (string->number lexeme))
+          (minHS-lexer input-port))]
+   [(:: #\# (:or #\t #\f))
+    ; =>
+    (cons (token-BOOL (equal? lexeme "#t"))
+          (minHS-lexer input-port))]
+   ;; TERMINA constantes
+
+
+   ;; Variables
+   [(:+ (:: (char-range #\a #\z) (:? (char-range #\0 #\9))))
+    ; =>
+    (cons (token-VAR (string->symbol lexeme))
+          (minHS-lexer input-port))]
+
+   
+   ;; EMPIEZA parentesis y brackets
    [#\(
     ;=>
     (cons (token-LP)
           (minHS-lexer input-port))]
-
    [#\)
     ;=>
     (cons (token-RP)
           (minHS-lexer input-port))]
 
+   [#\{
+    ;=>
+    (cons (token-LB)
+          (minHS-lexer input-port))]
+   [#\}
+    ;=>
+    (cons (token-RB)
+          (minHS-lexer input-port))]
+   
    [#\[
     ;=>
     (cons (token-LSB)
           (minHS-lexer input-port))]
-
    [#\]
     ;=>
     (cons (token-RSB)
           (minHS-lexer input-port))]
+   ;; TERMINA parentesis y brackets
 
-   [whitespace ;Caso expecial
+
+   ;; Casos especiales
+   [whitespace
     ; =>
-    (minHS-lexer input-port)] ;borramos todos los posibles espacios en blanco, tabuladores, etc
-
-   [(eof) ;Token que indica que se termino de lexear la cadena
+    (minHS-lexer input-port)] ; borramos todos los posibles espacios en blanco, tabuladores, etc
+   [(eof) ; Token que indica que se termino de lexear la cadena
     (list (token-EOF))]))
 
 ; Empecemos a definir la gramatica de minHS
@@ -163,13 +216,16 @@ Lexer y parser
 ; e :: = num | x | bool | opu(e) | opb(e,e) | fun [(x:T)]* e | ...
 
 ; Experimentos bonitos y romanticos
-;(let ([input (open-input-string "(3 - 33 + 6)")])
-;  (minHS-lexer input))
-
 (let* ([input (open-input-file "EjemplitoChido.mhs")]
        [tokens (minHS-lexer input)])
   (begin
     (close-input-port input)
     tokens))
+
+(let ([input (open-input-string "(3 - 33 + 6)")])
+  (minHS-lexer input))
+
+(let ([input (open-input-string "fun ([x:Bool][y:Func Bool Bool]:Bool) => if (#t and #t) then {hola} else {adios}")])
+  (minHS-lexer input))
 
 ; Proximamente un parser
