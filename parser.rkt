e3b6cceb84d11014c27e4911d6f21b8743c09d91
Raúl González <gonzalv@ciencias.unam.mx>
2020-10-23 22:51:39 -0500

    Agrega operadores and y or al parser

--- parser.rkt
+++ parser.rkt
@@ -68,7 +68,9 @@ Lexer y parser
    (precs
     (left APPT) ; precs clause. Here we can give some precedence of our language operators.
     (left ASSIGN)
-    (left TYPEOF)        
+    (left TYPEOF)
+    (left OR)
+    (left AND)
     (left - +) 
     (left * /))
    (grammar ; grammar clause. Here goes the grammar of minHS.
@@ -85,6 +87,9 @@ Lexer y parser
      [(exp - exp) (make-prim-exp - $1 $3)]
      [(exp * exp) (make-prim-exp * $1 $3)]
      [(exp / exp) (make-prim-exp / $1 $3)]
+     [(exp / exp) (make-prim-exp / $1 $3)]
+     [(exp AND exp) (make-prim-exp 'and $1 $3)]
+     [(exp OR exp) (make-prim-exp 'or $1 $3)]
 
      ;; If then else expression
      [(IF LP exp RP THEN LK exp RK ELSE LK exp RK) (if-then-exp $3 $7 $11)]
@@ -159,8 +164,8 @@ Desired response:
 (fun-exp (typeof-exp (brack-exp (typeof-exp (var-exp 'f) (func-exp (par-exp (func-exp (int-exp) (boole-exp))) (int-exp)))) (boole-exp)) (bool-exp #t))
 |#
 
-(display "\nExample 5.1: fun ([a:Bool][b:Bool]:Bool) => #t\n")
-(let ((input (open-input-string "fun ([a:Bool][b:Bool]:Bool) => #t")))
+(display "\nExample 5.1: fun ([a:Bool][b:Bool]:Bool) => a or b and c and d\n")
+(let ((input (open-input-string "fun ([a:Bool][b:Bool]:Bool) => a or b and c and d")))
   (minHS-parser (lex-this minHS-lexer input)))
 #|
 Desired response:
@@ -171,7 +176,13 @@ Desired response:
     (typeof-exp (var-exp 'a) (boole-exp))
     (typeof-exp (var-exp 'b) (boole-exp))))
   (boole-exp))
- (bool-exp #t))
+ (prim-exp
+  'or
+  (var-exp 'a)
+  (prim-exp
+   'and
+   (prim-exp 'and (var-exp 'b) (var-exp 'c))
+   (var-exp 'd))))
 |#
 
 (display "\nExample 6: funF (sumita ([x:Int][y:Int]):Int) => x+y\n")
